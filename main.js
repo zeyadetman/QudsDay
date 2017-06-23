27efaac335e203170e1247d92554c521019eb73d
Zeyad Etman <zeyadetman@yahoo.com>
2017-06-24 02:19:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 var i =-1;
-var bgi = ["1.jpg", "2.jpg","3.jpg","4.jpg"];
-var time = 6000;
+var bgi = ["Images/1.jpg", "Images/2.jpg","Images/3.jpg","Images/4.jpg"];
+var time = 6000
 
 
 window.setInterval(chgBG, time);
 
-function chgBG(){
+function chgBG(arr){
     i++;
     console.log(i%2);
     bG = bgi[i%bgi.length].toString();
     console.log(bG.toString());
-    document.getElementById('pgHeader').style.backgroundImage = "url(Images/" + bG + ")";
+    document.getElementById('header').style.backgroundImage = "url('"+bG+"')";
 }
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
