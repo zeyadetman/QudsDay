359344f5c870ccfbd5b3ef3ef1603f80c39be918
ibrahim hasan <elmasrye67@hotmail.com>
2017-06-23 18:50:11 +0200

    columns added and styled in the css sheet and the photos wrapped together in the directory Images.
    Signed-off-by: ibrahim hasan <elmasrye67@hotmail.com>

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var i =-1;
 var bgi = ["1.jpg", "2.jpg","3.jpg","4.jpg"];
-var time = 6000
+var time = 6000;
 
 
 window.setInterval(chgBG, time);
 
-function chgBG(arr){
+function chgBG(){
     i++;
     console.log(i%2);
     bG = bgi[i%bgi.length].toString();
     console.log(bG.toString());
-    document.getElementById('header').style.backgroundImage = "url('"+bG+"')";
+    document.getElementById('pgHeader').style.backgroundImage = "url(Images/" + bG + ")";
 }
